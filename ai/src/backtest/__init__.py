2bec7661ff19fa141416a7e341754f459fc17a70
chulsu0012 <chulsu0012@gmail.com>
2026-05-13 14:36:30 +0900

    feat: MVO (Mean-Variance Optimization) 구현

--- ai/src/backtest/__init__.py
+++ ai/src/backtest/__init__.py
@@ -1,5 +1,6 @@
 from .metrics import PerformanceMetrics, compute_metrics
 from .walk_forward import WalkForwardBacktest, WalkForwardConfig, WalkForwardResult, FoldMetrics
+from .mvo import MVO, MVOConfig, run_mvo_walk_forward
 
 __all__ = [
     "PerformanceMetrics",
@@ -8,4 +9,7 @@ __all__ = [
     "WalkForwardConfig",
     "WalkForwardResult",
     "FoldMetrics",
+    "MVO",
+    "MVOConfig",
+    "run_mvo_walk_forward",
 ]
